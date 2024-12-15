cff762eb17631a3c6c670831cfa7bf468eff8174
Luca Bosch <me@lucabosch.com>
2024-12-15 04:22:36 +0100

    Trade centers, move speed

---
+++ common/defines/ger_faster_move_speed.lua
@@ -0,0 +1,6 @@
+-- Since the map is only Germany, the provice size increased a lot. So people's speed is also increased to stay realistic.
+NDefines.NDiplomacy.DIPLOMAT_SPEED = 50.0
+NDefines.NEconomy.MERCHANT_SPEED = 50.0
+NDefines.NMilitary.INFANTRY_SPEED = 3.0
+NDefines.NMilitary.CAVALRY_SPEED = 3.0
+NDefines.NMilitary.ARTILLERY_SPEED = 3.0	
